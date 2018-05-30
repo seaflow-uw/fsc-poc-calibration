e1046163215991b18a48b688ee43789fe0683ee2
Francois Ribalet <ribalet@uw.edu>
2018-05-29 17:45:42 -0700

    update calibration for #751

--- analysis_influx.R
+++ analysis_influx.R
@@ -112,7 +112,7 @@ summary.table <- rbind(summary.table, table)
 
 }
 
-write.csv(summary.table,file=paste("influx-summary.csv", sep=""), row.names=FALSE)
+write.csv(summary.table,file=paste("influx_raw_summary.csv", sep=""), row.names=FALSE)
 
 
 
@@ -123,17 +123,17 @@ write.csv(summary.table,file=paste("influx-summary.csv", sep=""), row.names=FALS
 
 
 
-#########################
-### 3. MERGE with LOG ###
-#########################
+#############################
+#### 3. FSC NORMALIZATION ###
+#############################
 path.to.git.repository <- "~/Documents/DATA/Codes/fsc-poc-calibration"
-setwd(path.to.git.repository)
-summary.table <- read.csv(file=paste("influx-summary.csv", sep=""))
+path.to.data <- "~/Documents/DATA/Codes/fsc-poc-calibration/fsc-poc-calibration-data"
+setwd(path.to.data)
+summary.table <- read.csv(file=paste("influx_raw_summary.csv", sep=""))
 
 volume <- c(rep(500.3,2), rep(500.5,2),rep(496,2),rep(500.12,2),rep(300.3,2),rep(300.22,2),rep(300.45,2), rep(300,2), rep(150.46,2), rep(200.18,2),rep(150.2,2), rep(200.1,2),rep(300.28,2),rep(302,2),rep(200.57,2),rep(199.97,2),rep(300.25,2),
             rep(300.24,2),rep(99.9,2),rep(100.13,2),rep(99.87,2),rep(99.84,2),rep(100.05,2),rep(99.98,2),rep(100.2,2),rep(100.2,2),rep(99.93,2),rep(99.9,2),rep(100.07,2),rep(100.07,2))
 name <- rep(c("EHux","Licmo","Micro","Navicula","PT-632","PT-632","TAPS-1335","TAPS-3367","TW-3365","Pro 1314", "Syn 7803", "Pro AS9601", "Pro Med4","Pro Nat12A", "Syn WH8102"),each=4)
-
 
 ### CALCULATE CELL ABUNDANCE
 summary.table$volume.uL <- volume
@@ -144,26 +144,8 @@ summary.table$abundance_cells.mL[37:60] <- summary.table$abundance_cells.mL[37:6
 summary.table$abundance_cells.mL[9:12] <- summary.table$abundance_cells.mL[9:12] * 5
 
 
-
-write.csv(summary.table,file=paste("influx-stats.csv", sep=""), row.names=FALSE)
-
-
-
-
-
-
-
-
-
-#############################
-#### 4. FSC NORMALIZATION ###
-#############################
-path.to.git.repository <- "~/Documents/DATA/Codes/fsc-poc-calibration"
-setwd(path.to.git.repository)
-summary <- read.csv(file=paste("influx-stats.csv", sep=""))
-
-beads <- subset(summary, i == 'beads')
-cultures <- subset(summary, i == 'picoeuk')
+beads <- subset(summary.table, i == 'beads')
+cultures <- subset(summary.table, i == 'picoeuk')
 print(paste("Does rows of 'beads' and rows of 'cultures' match?",unique(cultures$file == beads$file)))
 cultures$norm.fsc <- round(cultures$fsc/beads$fsc,2)
 cultures$norm.chl <- round(cultures$fsc/beads$chl,2)
@@ -173,7 +155,7 @@ write.csv(cultures[,c("file","n","volume.uL","abundance_cells.mL","norm.fsc", "n
 
 
 ###########################################
-### 5. MERGE FCM data with POC/PON data ###
+### 4. MERGE FCM data with POC/PON data ###
 ###########################################
 path.to.git.repository <- "~/Documents/DATA/Codes/fsc-poc-calibration"
 setwd(path.to.git.repository)
@@ -181,30 +163,27 @@ poc <- read.csv("poc-data.csv")
 cultures <- read.csv("influx-cultures.csv")
 cultures$Sample.ID <- c(rep("EHUX",2), rep("LICMO",2), rep("MICRO",2),rep("NAV", 2), rep("PT 632",4), rep("TAPS 1135",2), rep("TAPS 3367",2), rep("TW 3365",2), rep("1314",2), rep("7803",2), rep("AS9601",2), rep("MED4",2),rep("NAT12A",2),rep("WH8102",2))
 
-write.csv(cultures,file=paste("~/Documents/DATA/Codes/fsc-poc-calibration/influx-cultures.csv", sep=""), row.names=FALSE)
-
 poc.sd <- aggregate(poc, by=list(poc$Sample.ID), FUN=sd)
-poc <- aggregate(poc, by=list(poc$Sample.ID), FUN=mean)
-poc$Sample.ID <- poc$Group.1
-poc$Date <- poc$Group.2
-poc$C..ug.ml.sd <- poc.sd$C..ug.ml.
-poc$N..ug.ml.sd <- poc.sd$N..ug.ml.
+poc.mean <- aggregate(poc, by=list(poc$Sample.ID), FUN=mean)
+poc.mean$Sample.ID <- poc.mean$Group.1
+poc.mean$C..ug.ml.sd <- poc.sd$C..ug.ml.
+poc.mean$N..ug.ml.sd <- poc.sd$N..ug.ml.
 
 cultures.sd <- aggregate(cultures, by=list(cultures$Sample.ID), FUN=sd)
-cultures <- aggregate(cultures, by=list(cultures$Sample.ID), FUN=mean)
-cultures$Sample.ID <- cultures$Group.1
-cultures$abundance_cells.mL.sd <- cultures.sd$abundance_cells.mL
-cultures$norm.fsc.sd <- cultures.sd$norm.fsc
-cultures$norm.chl.sd <- cultures.sd$norm.chl
+cultures.mean <- aggregate(cultures, by=list(cultures$Sample.ID), FUN=mean)
+cultures.mean$Sample.ID <- cultures.mean$Group.1
+cultures.mean$abundance_cells.mL.sd <- cultures.sd$abundance_cells.mL
+cultures.mean$norm.fsc.sd <- cultures.sd$norm.fsc
+cultures.mean$norm.chl.sd <- cultures.sd$norm.chl
 
 ### MERGE POC with Cell Abundance
-merge <- merge(poc, cultures, by='Sample.ID')
+merge <- merge(poc.mean, cultures.mean, by='Sample.ID')
 
 ### Calculate Quotas
 merge$pgC.cell <- 10^6*(merge$C..ug.ml.)/(merge$abundance_cells.mL) # pgC.cell-1
 merge$pgN.cell <- 10^6*(merge$N..ug.ml.)/(merge$abundance_cells.mL) # pgN.cell-1
-merge$pgC.cell.sd <- sqrt((merge$C..ug.ml.sd/merge$C..ug.ml.)^2 + (merge$abundance_cells.mL.sd/merge$abundance_cells.mL)^2)
-merge$pgN.cell.sd <- sqrt((merge$N..ug.ml.sd/merge$N..ug.ml.)^2 + (merge$abundance_cells.mL.sd/merge$abundance_cells.mL)^2)
+merge$pgC.cell.sd <- merge$pgC.cell * sqrt((merge$C..ug.ml.sd/merge$C..ug.ml.)^2 + (merge$abundance_cells.mL.sd/merge$abundance_cells.mL)^2)
+merge$pgN.cell.sd <- merge$pgN.cell * sqrt((merge$N..ug.ml.sd/merge$N..ug.ml.)^2 + (merge$abundance_cells.mL.sd/merge$abundance_cells.mL)^2)
 
 ### CELL QUOTAS vs NORM FSC for INFLUX
 write.csv(merge[,c("Sample.ID","norm.fsc","norm.fsc.sd","norm.chl","norm.chl.sd","abundance_cells.mL","abundance_cells.mL.sd","pgC.cell","pgN.cell","pgC.cell.sd","pgN.cell.sd")],file="Influx-Qc-cultures.csv", row.names=FALSE)
@@ -215,7 +194,7 @@ write.csv(merge[,c("Sample.ID","abundance_cells.mL","abundance_cells.mL.sd","pgC
 
 
 ############################
-### 6. LINEAR REGRESSION ###
+### 5. LINEAR REGRESSION ###
 ############################
 library(scales)
 .rainbow.cols <- colorRampPalette(c("#00007F", "blue", "#007FFF", "cyan", "#7FFF7F", "yellow","#FF7F00", "red", "#7F0000"))
@@ -230,9 +209,10 @@ merge <- read.csv("Influx-Qc-cultures.csv")
 merge2 <- subset(merge, Sample.ID !="PT 632" & Sample.ID !="EHUX" & Sample.ID !="LICMO")
 merge2 <- merge2[order(merge2$norm.fsc),]
 
-# linear regression type II
+# linear regression
 reg <- lm(pgC.cell ~ norm.fsc, data=log(merge2[,c("pgC.cell","norm.fsc")],10))
 
+reg <- lm(pgC.cell ~ norm.fsc, data=merge2[,c("pgC.cell","norm.fsc")])
 
 png("Influx-Qc-scatter.png",width=12, height=12, unit='in', res=100)
 
@@ -250,3 +230,17 @@ legend("topleft", legend=bquote(paste("Qc=",.(round(10^reg$coefficients[1],3)),"
 legend("bottomright", legend=merge2$Sample.ID, pt.bg=alpha(.rainbow.cols(nrow(merge2)),0.5),pch=21,bty='n')
 
 dev.off()
+
+
+
+reg <- lm(pgC.cell ~ norm.fsc, data=merge2[,c("pgC.cell","norm.fsc")])
+par(mfrow=c(1,1), pty='s',cex=1.4)
+plot(merge2$norm.fsc,merge2$pgC.cell,  yaxt='n',cex=2,bg=alpha(.rainbow.cols(nrow(merge2)),0.5), pch=21,ylab=expression(paste("Qc (pgC cell"^{-1},")")), xlab="Normalized scatter (dimensionless)", main='Influx')
+with(merge2, arrows(norm.fsc, pgC.cell - merge2$pgC.cell.sd, norm.fsc, pgC.cell + merge2$pgC.cell.sd,  code = 3, length=0))
+with(merge2, arrows(norm.fsc-norm.fsc.sd, pgC.cell, norm.fsc+norm.fsc.sd, pgC.cell,  code = 3, length=0))
+axis(2, at=c(0.1,1,10,100,1000), labels=c(0.1,1,10,100,1000))
+lines(x=merge2$norm.fsc,predict(reg, newdata=data.frame(norm.fsc=merge2$norm.fsc),interval='predict')[,"fit"], col='red3',lwd=2 )
+lines(x=merge2$norm.fsc,predict(reg, newdata=data.frame(norm.fsc=merge2$norm.fsc),interval='predict')[,"lwr"], col='grey',lwd=2 )
+lines(x=merge2$norm.fsc,predict(reg, newdata=data.frame(norm.fsc=merge2$norm.fsc),interval='predict')[,"upr"], col='grey',lwd=2 )
+legend("topleft", legend=bquote(paste("Qc=",.(round(reg$coefficients[2],3)),"(scatter)",.(round(reg$coefficients[1],3)))), bty='n',cex=2)
+legend("bottomright", legend=merge2$Sample.ID, pt.bg=alpha(.rainbow.cols(nrow(merge2)),0.5),pch=21,bty='n')
--- analysis_seaflow.R
+++ analysis_seaflow.R
@@ -2,6 +2,7 @@ library(flowCore)
 library(splancs)
 library(plotrix)
 library(caroline)
+library(popcycle)
 .rainbow.cols <- colorRampPalette(c("#00007F", "blue", "#007FFF", "cyan", "#7FFF7F", "yellow","#FF7F00", "red", "#7F0000"))
 
 #####################
@@ -18,137 +19,153 @@ library(popcycle)
 path.to.data <- "~/Documents/DATA/Codes/fsc-poc-calibration/fsc-poc-calibration-data"
 setwd(path.to.data)
 
-file.list <- dir(".", pattern = "00-00$", recursive=T, full.names=T); inst <- 740
+file.list <- dir("740_Small_cyano", pattern = "00-00$", recursive=T, full.names=T); inst <- 740 # small
+file.list <- dir("740_Large_picoeuks", pattern = "00-00$", recursive=T, full.names=T); inst <- 740 # large
 file.list <- dir(".", pattern = "07-00$", recursive=T, full.names=T); inst <- 751 # small
 file.list <- dir(".", pattern = "08-00$", recursive=T, full.names=T); inst <- 751 # large
 
-
+# gating
 summary.table <- NULL
 draw.gate <- TRUE
 
-for (file in file.list) {
-    print(paste("processing file:",file))
-#file <- file.list[20]
+    for (file in file.list[c(9:12)]) {
+        print(paste("processing file:",file))
+    #file <- file.list[4]
 
-### read EVT
-evt <- readSeaflow(file, transform=T)
-evt <- evt[evt$fsc_small > 1 | evt$D1 > 1 | evt$D2 > 1, ]
-evt <- subset(evt, D2 < D1 + 2500 & D1 < D2 + 2500)
+    ### read EVT and get OPP
+    evt <- readSeaflow(file, transform=T)
+    evt <- subset(evt, fsc_small > 2)
+    evt <- evt[round(seq(1,nrow(evt), length.out=100000)),]
+    ip <- inflection.point(evt)
+    filter.params <- create.filter.params(inst, fsc=ip$fsc, d1=ip$d1, d2=ip$d2)
+    opp <- filter.notch(evt, filter.params[2,])$opp
+    opp$pop <- 0
 
-evt$pop <- 0
-
-### GATING
-#1. BEADS
-x <- subset(evt, fsc_small > 2 & pe > 5)
-if(draw.gate) plot.cytogram(x, "fsc_small","pe", main="NOISE & BEADS & SYN")
-print("Gating Beads")
-if(draw.gate) poly.beads <- getpoly(quiet=TRUE)
-beads <- subset(x,inout(x[,c("fsc_small","pe")],poly=poly.beads, bound=TRUE, quiet=TRUE))
-evt[row.names(beads),'pop'] <- "beads"
+    ### GATING
+    #1. BEADS
+    x <- subset(opp, pop==0)
+    if(draw.gate) plot.cytogram(x, "fsc_small","pe", main="NOISE & BEADS & SYN")
+    print("Gating Beads")
+    if(draw.gate) poly.beads <- getpoly(quiet=TRUE)
+    beads <- subset(x,inout(x[,c("fsc_small","pe")],poly=poly.beads, bound=TRUE, quiet=TRUE))
+    opp[row.names(beads),'pop'] <- "beads"
 
 
-#2. cultures
-x <- subset(evt, pop==0 & fsc_small > 2)
-plot.cytogram(x, "fsc_small","chl_small", main="Pico")
-points(beads$fsc_small, beads$chl_small, cex=0.3, col=2, pch=16)
-print("gating PICO")
-if(draw.gate) poly.pico <- getpoly(quiet=TRUE)
-pico <- subset(x,inout(x[,c("fsc_small","chl_small")],poly=poly.pico, bound=TRUE, quiet=TRUE))
-evt[row.names(pico),'pop'] <- "picoeuk"
+    #2. cultures
+    x <- subset(opp, pop==0)
+    plot.cytogram(x, "chl_small", "pe", main="Pico")
+    points(beads$fsc_small, beads$chl_small, cex=0.3, col=2, pch=16)
+    print("gating PICO")
+    if(draw.gate) poly.pico <- getpoly(quiet=TRUE)
+    pico <- subset(x,inout(x[,c("chl_small","pe")],poly=poly.pico, bound=TRUE, quiet=TRUE))
+    opp[row.names(pico),'pop'] <- "picoeuk"
 
-### SAVE PLOT
-png(paste0(file,".png"),width=9, height=12, unit='in', res=100)
-par(mfrow=c(2,2))
-plot.vct.cytogram(evt, "fsc_small","chl_small")
-plot.vct.cytogram(evt, "fsc_small","pe")
-plot.vct.cytogram(evt, "chl_small","pe")
-#plot.vct.cytogram(evt, "fsc_small","fsc_perp")
-dev.off()
+    ### SAVE PLOT
+    png(paste0(file,".png"),width=9, height=12, unit='in', res=100)
+    par(mfrow=c(2,2))
+    plot.vct.cytogram(opp, "fsc_small","chl_small")
+    plot.vct.cytogram(opp, "fsc_small","pe")
+    plot.vct.cytogram(opp, "chl_small","pe")
+    #plot.vct.cytogram(evt, "fsc_small","fsc_perp")
+    dev.off()
 
-### SUMMARY
-stat.table <- NULL
-for(i in unique(evt$pop)){
-#print(i)
-  if(i == 0) next
-  p <- subset(evt, pop == i)
-  n <- nrow(p)
-  if(n ==0) {
-      fsc <- 0
-      chl <- 0
-          }else{
-      fsc <- round(median(p$fsc_small))
-      chl <- round(median(p$chl_small))
-      pe <- round(median(p$pe))
-      }
-    var <- cbind(i,n,fsc,chl,pe)
-    stat.table <- rbind(stat.table, var)
+    ### SUMMARY
+    stat.table <- NULL
+    for(i in unique(opp$pop)){
+    #print(i)
+      if(i == 0) next
+      p <- subset(opp, pop == i)
+      n <- nrow(p)
+      if(n ==0) {
+          fsc <- 0
+          chl <- 0
+              }else{
+          fsc <- round(median(p$fsc_small))
+          chl <- round(median(p$chl_small))
+          pe <- round(median(p$pe))
+          }
+        var <- cbind(i,n,fsc,chl,pe)
+        stat.table <- rbind(stat.table, var)
+    }
+
+    table <- data.frame(cbind(stat.table, file=basename(file)))
+    summary.table <- rbind(summary.table, table)
+
+    }
+
+write.csv(summary.table,file=paste0(unique(dirname(file.list)),"/",inst,"_raw_summary.csv", sep=""), row.names=FALSE)
+
+
+
+### rbind SMALL & LARGE data
+
+for(inst in c(740,751)){
+    print(inst)
+    list <- dir(".", pattern = paste0(inst,"_raw_summary.csv"), recursive=T, full.names=T)
+
+    DF <- NULL
+    for(file in list){
+        df <- read.csv(file)
+        DF <- rbind(DF, df)
+    }
+    time <- as.POSIXct(DF$file, format = "%FT%H-%M-%S", tz = "GMT")
+    DF[order(time),]
+    write.csv(DF,file=paste0(inst, "-summary.csv", sep=""), row.names=FALSE)
+
 }
-
-table <- data.frame(cbind(stat.table, file=basename(file)))
-summary.table <- rbind(summary.table, table)
-
-}
-
-write.csv(summary.table,file=paste0(inst, "-summary2.csv", sep=""), row.names=FALSE)
-
-
-
 
 #############################
 #### 3. FSC NORMALIZATION ###
 #############################
 path.to.git.repository <- "~/Documents/DATA/Codes/fsc-poc-calibration"
-setwd(path.to.git.repository)
 
-### CHOOSE SeaFlow serial number
-inst <- 740
-inst <- 751
+for(inst in c(740,751)){
 
-summary <- read.csv(file=paste0(inst,"-summary.csv", sep=""))
-beads <- subset(summary, i == 'beads')
-cultures <- subset(summary, i == 'picoeuk')
+  summary <- read.csv(file=paste0(inst,"-summary.csv", sep=""))
+  beads <- subset(summary, i == 'beads')
+  cultures <- subset(summary, i == 'picoeuk')
 
-print(paste("Does rows of beads and cultures match?",unique(cultures$file == beads$file)))
-cultures$norm.fsc <- round(cultures$fsc/beads$fsc,2)
-cultures$norm.chl <- round(cultures$fsc/beads$chl,2)
+  print(paste("Does rows of beads and cultures match?",unique(cultures$file == beads$file)))
+  cultures$norm.fsc <- round(cultures$fsc/beads$fsc,2)
+  cultures$norm.chl <- round(cultures$fsc/beads$chl,2)
 
 
-write.csv(cultures,file=paste0(inst,"-cultures.csv", sep=""), row.names=FALSE)
+  write.csv(cultures,file=paste0(path.to.git.repository,"/",inst,"-cultures.csv", sep=""), row.names=FALSE)
 
-
+}
 
 ###########################################
 ### 4. MERGE FCM data with POC/PON data ###
 ###########################################
 path.to.git.repository <- "~/Documents/DATA/Codes/fsc-poc-calibration"
 setwd(path.to.git.repository)
-
-### CHOOSE SeaFlow serial number
-inst <- 740
-inst <- 751
-
-
 poc <- read.csv("Qc-cultures.csv")
-cultures <- read.csv(paste0(inst,"-cultures.csv"))
 
-if(inst == 740){ cultures$Sample.ID <- c(rep("TW 3365",2),"-" ,rep("NAV",2), "-" ,rep("TAPS 1135",2), rep("-",2),rep("TAPS 3367",2), "-" ,rep("PT 632",2), "-" ,rep("MICRO",2),"-" ,
-                                          rep("MED4",2),rep("-",3), rep("AS9601",2),"-" , rep("1314",2),"-" ,rep("MED4",2),"-" ,rep("NAT12A",2),"-" ,rep("WH8102",2), "-" ,rep("7803",2),"-")
-          }
-if(inst == 751){ cultures$Sample.ID <- c(rep("MED4",2), "-", rep("AS9601",2),"-" , rep("1314",2),"-" ,rep("NAT12A",2),"-" ,rep("WH8102",2), "-" ,rep("7803",2),"-",
-                                          rep("TW 3365",2),"-" ,rep("NAV",2), "-" ,rep("TAPS 1135",2), rep("-",2),rep("TAPS 3367",2), "-" ,rep("PT 632",2), rep("-",3),rep("MICRO",2),"-")
-          }
+for(inst in c(740,751)){
 
-cultures.sd <- aggregate(cultures, by=list(cultures$Sample.ID), FUN=sd)
-cultures <- aggregate(cultures, by=list(cultures$Sample.ID), FUN=mean)
-cultures$Sample.ID <- cultures$Group.1
-cultures$norm.fsc.sd <- cultures.sd$norm.fsc
-cultures$norm.chl.sd <- cultures.sd$norm.chl
+    cultures <- read.csv(paste0(inst,"-cultures.csv"))
+
+    if(inst == 740){ cultures$Sample.ID <- c(rep("TW 3365",2),"-" ,rep("NAV",2), "-" ,rep("TAPS 1135",2), rep("-",2),rep("TAPS 3367",2), "-" ,rep("PT 632",2), "-" ,rep("MICRO",2),"-" ,
+                                              rep("MED4",2),rep("-",3), rep("AS9601",2),"-" , rep("1314",2),"-" ,rep("MED4",2),"-" ,rep("NAT12A",2),"-" ,rep("WH8102",2), "-" ,rep("7803",2),"-")
+              }
+    if(inst == 751){ cultures$Sample.ID <- c(rep("TW 3365",2),rep("NAV",2), rep("TAPS 1135",2), rep("TAPS 3367",2),rep("PT 632",2), rep("MICRO",2),
+                                              rep("MED4",2), rep("AS9601",2),rep("1314",2),rep("NAT12A",2), rep("WH8102",2), rep("7803",2))
+              }
+
+    cultures.sd <- aggregate(cultures, by=list(cultures$Sample.ID), FUN=sd)
+    cultures.mean <- aggregate(cultures, by=list(cultures$Sample.ID), FUN=mean)
+    cultures.mean$Sample.ID <- cultures.mean$Group.1
+    cultures.mean$norm.fsc.sd <- cultures.sd$norm.fsc
+    cultures.mean$norm.chl.sd <- cultures.sd$norm.chl
 
 
-### MERGE POC with Cell Abundance
-merge <- merge(poc, cultures, by='Sample.ID')
+    ### MERGE POC with Cell Abundance
+    merge <- merge(poc, cultures.mean, by='Sample.ID')
 
-write.csv(merge[,c("Sample.ID","norm.fsc","norm.fsc.sd","norm.chl","norm.chl.sd","abundance_cells.mL","abundance_cells.mL.sd","pgC.cell","pgN.cell","pgC.cell.sd","pgN.cell.sd")],file=paste0(inst,"-Qc-cultures.csv"), row.names=FALSE)
+    write.csv(merge[,c("Sample.ID","norm.fsc","norm.fsc.sd","norm.chl","norm.chl.sd","abundance_cells.mL","abundance_cells.mL.sd","pgC.cell","pgN.cell","pgC.cell.sd","pgN.cell.sd")],file=paste0(inst,"-Qc-cultures.csv"), row.names=FALSE)
+
+}
+
 
 
 ############################
@@ -167,12 +184,12 @@ inst <- 751
 merge <- read.csv(paste0(inst,"-Qc-cultures.csv"))
 
 
-merge2 <- subset(merge, Sample.ID !="PT 632")
+merge2 <- subset(merge, Sample.ID !="PT 632" & Sample.ID !="TAPS 3367" & Sample.ID !="TAPS 1135" & Sample.ID !="NAV")
+
 merge2 <- merge2[order(merge2$norm.fsc),]
 
 # linear regression type II
 reg <- lm(pgC.cell ~ norm.fsc, data=log(merge2[,c("pgC.cell","norm.fsc")],10))
-
 save(reg, file=paste0("lm_",inst))
 
 
@@ -180,7 +197,7 @@ png(paste0(inst,"-Qc-scatter.png"),width=12, height=12, unit='in', res=100)
 
 par(mfrow=c(1,1), pty='s',cex=1.4)
 plot(merge2$norm.fsc,merge2$pgC.cell, log='xy', yaxt='n',cex=2,bg=alpha(.rainbow.cols(nrow(merge2)),0.5), pch=21,ylab=expression(paste("Qc (pgC cell"^{-1},")")), xlab="Normalized scatter (dimensionless)", main=paste("#",inst))
-with(merge2, arrows(norm.fsc, pgC.cell - merge2$pgC.cell.sd, norm.fsc, pgC.cell + merge2$pgC.cell.sd,  code = 3, length=0))
+with(merge2, arrows(norm.fsc, pgC.cell - pgC.cell.sd, norm.fsc, pgC.cell + pgC.cell.sd,  code = 3, length=0))
 with(merge2, arrows(norm.fsc-norm.fsc.sd, pgC.cell, norm.fsc+norm.fsc.sd, pgC.cell,  code = 3, length=0))
 axis(2, at=c(0.1,1,10,100,1000), labels=c(0.1,1,10,100,1000))
 par(new=T)
