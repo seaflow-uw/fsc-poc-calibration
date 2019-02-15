8694c63640f365f61b7b8403acc0dafc75407588
Francois Ribalet <ribalet@uw.edu>
2019-02-15 14:11:19 -0800

    new plot

--- analysis_seaflow.R
+++ analysis_seaflow.R
@@ -170,10 +170,10 @@ library(viridis)
 path.to.git.repository <- "~/Documents/DATA/Codes/fsc-poc-calibration"
 setwd(path.to.git.repository)
 
-### CHOOSE SeaFlow serial number
-inst <- 740
-inst <- 751
+png("Qc-scatter.png",width=12, height=6, unit='in', res=200)
+par(mfrow=c(1,2), pty='s',cex=1.2)
 
+for(inst in c(740, 751)){
 
 merge <- read.csv(paste0(inst,"-Qc-cultures.csv"))
 merge2 <- subset(merge, Sample.ID !="Phaeodactylum tricornutum") # remove non-spherical cells
@@ -181,10 +181,8 @@ merge2 <- merge2[order(merge2$norm.fsc),]
 
 mie <- read.csv("calibrated-mie.csv")
 
-png(paste0(inst,"-Qc-scatter.png"),width=6, height=6, unit='in', res=200)
 
-par(mfrow=c(1,1), pty='s',cex=1.2)
-plot(merge2$norm.fsc,merge2$pgC.cell, log='xy', yaxt='n', xaxt='n', pch=NA,xlim=c(0.002,10), ylim=c(0.005,100), ylab=expression(paste("Qc (pgC cell"^{-1},")")), xlab="Normalized scatter (dimensionless)", main=paste("#",inst))
+plot(merge2$norm.fsc,merge2$pgC.cell, log='xy', yaxt='n', xaxt='n', pch=NA,xlim=c(0.002,10), ylim=c(0.005,100), ylab=expression(paste("Qc (pgC cell"^{-1},")")), xlab="Normalized scatter (dimensionless)", main=paste(inst))
 with(merge2, arrows(norm.fsc, pgC.cell - pgC.cell.sd, norm.fsc, pgC.cell + pgC.cell.sd,  code = 3, length=0, col='grey', lwd=2))
 with(merge2, arrows(norm.fsc-norm.fsc.sd, pgC.cell, norm.fsc+norm.fsc.sd, pgC.cell,  code = 3, length=0,col='grey',lwd=2))
 lines(mie$scatter, mie[,paste0("Qc_",inst,"_mid")], col='red3', lwd=2)
@@ -195,5 +193,7 @@ axis(2, at=c(0.005,0.01,0.02,0.05,0.1,0.2,0.5,1,2,5,10,20,50,100,1000), labels=c
 axis(1, at=c(0.002,0.005,0.01,0.02,0.05,0.1,0.2,0.5,1,2,5,10),labels=c(0.002,0.005,0.01,0.02,0.05,0.1,0.2,0.5,1,2,5,10))
 legend("topleft",legend=c(as.vector(merge2$Sample.ID),"Mie-based model (n = 1.031 +/- 0.014)"), cex=0.5,pch=c(rep(21,nrow(merge2)),NA), lwd=c(rep(NA,nrow(merge2)),2), bty='n',
           pt.bg=alpha(viridis(nrow(merge2)),0.5), col=c(rep(1,nrow(merge2)),'red3'), text.font=c(rep(3,nrow(merge2)),1))
+
+}
 
 dev.off()
